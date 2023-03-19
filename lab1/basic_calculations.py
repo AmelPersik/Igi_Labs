93950a64bb2eb8e757127f81972f95e6ebb33775
amelpersik <st.dasha.st@gmail.com>
2023-03-19 23:21:55 +0300

    update file used functions

---
+++ lab1/basic_calculations.py
@@ -0,0 +1,6 @@
+from functions import basic_calculations
+
+print(basic_calculations(5,5,"add"))
+print(basic_calculations(5,5,"sub"))
+print(basic_calculations(5,6,"mul"))
+print(basic_calculations(5,0,"div"))
