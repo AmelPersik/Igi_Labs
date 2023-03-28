bc6a9d898ae4ef3e6ad4674d4d2ed3f1c5d284f8
amelpersik <st.dasha.st@gmail.com>
2023-03-28 17:13:23 +0300

    add task3

---
+++ Lab1/Task3.py
@@ -0,0 +1,2 @@
+def even_list(list_of_nums):
+    return [num for num in list_of_nums if num % 2 == 0]
--- Lab1/main.py
+++ Lab1/main.py
@@ -1,10 +1,15 @@
 from Task1 import print_hello_world
 from Task2 import basic_calculations
+from Task3 import even_list
 
 print("Task1: " + print_hello_world())
 
+print("\nTask2: ")
 print(basic_calculations(6, 7, "add"))
 print(basic_calculations(1, 2, "mult"))
-print(basic_calculations(0, 0, "div"))
+print(basic_calculations(8, 5, "sub"))
 print(basic_calculations(1, 0, "div"))
 print(basic_calculations(1, 0, "way"))
+
+print("\nTask3:")
+print(even_list([num for num in range(1, 50)]))
