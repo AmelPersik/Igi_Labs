f70a267622dca9e492f221c2b798ed2c29a9f2fa
amelpersik <st.dasha.st@gmail.com>
2023-03-28 16:43:13 +0300

    delete lab1

--- lab1/basic_calculations.py
+++
@@ -1,6 +0,0 @@
-from functions import basic_calculations
-
-print(basic_calculations(5,5,"add"))
-print(basic_calculations(5,5,"sub"))
-print(basic_calculations(5,6,"mul"))
-print(basic_calculations(5,0,"div"))
--- lab1/even_list.py
+++
@@ -1,3 +0,0 @@
-from functions import even_numbers_list
-
-print(even_numbers_list([num for num in range(1, 101)]))
