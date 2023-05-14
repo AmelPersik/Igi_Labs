93e137beab461b8a4d6ca69f84f4981987f7ab3b
amelpersik <st.dasha.st@gmail.com>
2023-05-15 00:41:14 +0300

    update const file

---
+++ Lab2/constants.py
@@ -0,0 +1,8 @@
+abbreviations = ['a.m.', 'p.m.', 'Mr.', 'Mrs.', 'Ms.', 'e.g.', 'Jan.', 'Feb.', 'Mar.', 'Apr.', 'Jun.', 'Jul.',
+                 'Aug.', 'Sep.', 'Oct.', 'Nov.', 'Dec.']
+regular_abbreviations = r'a\.m\.|p\.m\.|Mr\.|Mrs\.|Ms\.|e\.g\.|Jan\.|Feb\.|Mar\.|Apr\.|Jun\.|Jul\.|Aug\.|Sep\.' +\
+                        r'|Oct\.|Nov\.|Dec\.'
+
+regular_sentence = r'((([^\.\!\?]*('+regular_abbreviations+r'))*[^\.\?\!]*)[\.\?\!]+)'
+regular_word = r'\b[a-z0-9]*[a-z][a-z0-9]*\b'
+regular_sentence_declarative = r'[^?!]+\.+'
